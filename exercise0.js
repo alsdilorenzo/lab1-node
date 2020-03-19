822973b11e90a5f804b8986cc5d965795f6c9c72
alsdilorenzo <alsdlr@k501ux.localdomain>
2020-03-19 18:38:21 +0100

    completed exercise0 of the first lab

--- exercise0.js
+++ exercise0.js
@@ -1,3 +1,14 @@
 "use strict";
 
-// here goes the code
+let wordList = ["Spring", "Winter", "Autumn", "Summer", "Hi", "Test"];
+console.log(wordList)
+
+
+for(let [i, s] of wordList.entries()){
+    if(s.length <= 3)
+        wordList[i] = "";
+    else
+        wordList[i] = s.slice(0, 2) + s.slice(s.length-2, s.length);
+}
+
+console.log(wordList)
